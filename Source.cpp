76357a70eab7776b22dc1d60100de989fa129eb6
Nguyen Hoang Anh Vu <dh52112101@student.stu.edu.vn>
2024-11-02 08:46:24 +0700

    hieu

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b)
+int tong(int a, int b);
+int hieu(int a, int b)
 {
-	return a+b;
-}
-int hieu(int a, int b);
+	return a-b;
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
@@ -16,7 +15,7 @@ int main()
 	cin >> a;
 	cout << "nhap b: ";
 	cin >> b;
-	cout << "tong: " << tong(a,b);
+	cout << "hieu: " << hieu(a,b);
 	system("pause");
 	return 0;
 }
